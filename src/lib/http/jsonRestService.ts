b3b34bf768e18a8d996eccd600c11b18846f62a9
jaylco <77386599+jaylco@users.noreply.github.com>
2022-08-08 16:14:51 +0100

    more straightforward request logging

--- src/lib/http/jsonRestService.ts
+++ src/lib/http/jsonRestService.ts
@@ -1,8 +1,10 @@
 import * as url from 'url'
-import axios, {AxiosInstance, AxiosResponse} from 'axios'
+import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios'
 import {Auth} from '../../identity/auth'
+import { getLogger } from '../../utils/logger'
 
 export class JsonRestService {
+	logger = getLogger('JsonRestService')
 	private _http: AxiosInstance
 	config: any
 	auth: Auth
@@ -12,6 +14,17 @@ export class JsonRestService {
 		this._http = axios.create({
 			baseURL: config.url,
 			timeout: config.timeout,
+		})
+
+		this._http.interceptors.request.use((conf: AxiosRequestConfig) => {
+			const req = conf
+			let logMsg = `${req.method} request to ${req.url}.`
+			if (req.data) {
+				const stringedData = JSON.stringify(req.data)
+				logMsg += ` Data: ${stringedData}`
+			}
+			this.logger.debug(logMsg)
+			return conf
 		})
 
 		this.config = config
