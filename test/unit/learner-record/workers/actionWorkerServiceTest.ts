54a623d63b926048c2ba25e21c9fa8a0ea17c16b
jaylco <77386599+jaylco@users.noreply.github.com>
2023-09-08 10:19:34 +0100

    Build error

--- test/unit/learner-record/workers/actionWorkerServiceTest.ts
+++ test/unit/learner-record/workers/actionWorkerServiceTest.ts
@@ -1,17 +1,16 @@
-import * as chai from 'chai';
+import * as chai from 'chai'
 import {expect} from 'chai'
-import * as chaiAsPromised from 'chai-as-promised';
-import * as sinonChai from 'sinon-chai';
+import * as chaiAsPromised from 'chai-as-promised'
+import * as sinonChai from 'sinon-chai'
 
-import { CsrsService } from '../../../../src/csrs/service/csrsService';
-import { OrganisationalUnitService } from '../../../../src/csrs/service/organisationalUnitService';
-import { LearnerRecord } from '../../../../src/learner-record';
-import { ActionWorkerService } from '../../../../src/learner-record/workers/actionWorkerService';
-import {
-    CancelBookingActionWorker
-} from '../../../../src/learner-record/workers/cancelBookingActionWorker';
-import { WorkerAction } from '../../../../src/learner-record/workers/WorkerAction';
-import { LearningCatalogue } from '../../../../src/learning-catalogue';
+import {CsrsService} from '../../../../src/csrs/service/csrsService'
+import {OrganisationalUnitService} from '../../../../src/csrs/service/organisationalUnitService'
+import {LearnerRecord} from '../../../../src/learner-record'
+import {ActionWorkerService} from '../../../../src/learner-record/workers/actionWorkerService'
+import {CancelBookingActionWorker} from '../../../../src/learner-record/workers/cancelBookingActionWorker'
+import {WorkerAction} from '../../../../src/learner-record/workers/WorkerAction'
+import {LearningCatalogue} from '../../../../src/learning-catalogue'
+import {CslServiceClient} from '../../../../src/csl-service/client'
 
 chai.use(chaiAsPromised)
 chai.use(sinonChai)
@@ -23,8 +22,9 @@ describe('Tests for actionWorkerService', () => {
         const csrs = <CsrsService>{}
 		const learnerRecordApi = <LearnerRecord>{}
         const organisationalUnitService = <OrganisationalUnitService>{}
+        const cslService = <CslServiceClient>{}
 
-        const service = new ActionWorkerService(learningCatalogue, csrs, learnerRecordApi, organisationalUnitService)
+        const service = new ActionWorkerService(learningCatalogue, csrs, learnerRecordApi, organisationalUnitService, cslService)
         service.init()
         const worker = service.getWorker(WorkerAction.CANCEL_BOOKING)
         expect(worker).instanceOf(CancelBookingActionWorker)
