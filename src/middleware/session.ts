a01a43aa507a3d05fba1a7acb6fa94e16b343431
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-11-04 15:01:53 +0000

    Remove redundant env values. Fix redirect

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -5,8 +5,7 @@ import {AppConfig} from '../config/appConfig'
 import {RedisStore} from 'connect-redis'
 import * as cookieParser from 'cookie-parser'
 import * as connectRedis from 'connect-redis'
-import * as redis from 'redis'
-import * as config from '../config'
+import {redisClient} from 'lib/redis'
 
 export class SessionMiddleware extends Middleware {
     apply(app: Express): void {
@@ -33,12 +32,6 @@ export class SessionMiddleware extends Middleware {
 export const buildSessionMiddleware = () => {
 	const appConfig = new AppConfig()
 	const RedisStore = connectRedis(session)
-	const redisClient = redis.createClient({
-		auth_pass: config.REDIS.password,
-		host: config.REDIS.host,
-		no_ready_check: true,
-		port: config.REDIS.port,
-	})
 	const redisStore = new RedisStore({
 		client: redisClient,
 	})
