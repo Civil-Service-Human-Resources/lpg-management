ef69d990572afba073d4c3d19ad6793793c94697
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2025-07-24 12:36:39 +0100

    LC-3704: Remove express-fileupload library

--- src/middleware/requestMiddleware.ts
+++ src/middleware/requestMiddleware.ts
@@ -1,12 +1,9 @@
 import {Middleware} from './middleware'
 import {Express} from 'express'
 import * as bodyParser from 'body-parser'
-const fileUpload = require('express-fileupload')
 
 export class RequestMiddleware extends Middleware {
 	apply(app: Express): void {
-		app.use(fileUpload())
-
 		app.use(bodyParser.json())
 		app.use(bodyParser.urlencoded({extended: false}))
 	}
