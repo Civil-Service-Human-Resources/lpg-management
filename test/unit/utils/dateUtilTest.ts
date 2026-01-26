e51152ce8471ed00b02089e66c9ccebbab6dc99e
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-01-26 16:40:56 +0000

    tests

--- test/unit/utils/dateUtilTest.ts
+++ test/unit/utils/dateUtilTest.ts
@@ -13,7 +13,7 @@ import {getStartOfJs} from '../../../src/utils/dateUtil'
 describe('DateUtil tests', () => {
 	describe('getStartOfJs tests', () => {
 		it('Should return the dayjs with a timezone, when the startOf is a day', () => {
-			const start = getStartOfJs(dayjs('2024-04-01'), dayjs('2025-01-01'))
+			const start = getStartOfJs(dayjs('2024-04-01').tz('Europe/London'), dayjs('2025-01-01').tz('Europe/London'))
 			expect(start.utcOffset()).to.eql(60)
 		})
 		it('Should return the dayjs without a timezone, when the startOf is a month', () => {
