777915d021f67fb7a2912c7e629f3b16bb295a63
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-09-16 09:54:16 +0100

    Fix pagination tests

--- test/unit/pagination/paginationTest.ts
+++ test/unit/pagination/paginationTest.ts
@@ -22,78 +22,69 @@ describe('Pagination tests', function() {
         it('should return 1 as previous if currentPage is 2', function() {
             result.page = 1
             const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.previous!.href).to.equal('/content-management/search?p=1')
+            expect(paginationResult.prevLink).to.equal('/content-management/search?p=1')
         })
 
         it('should return null as previous if currentPage is 1', function() {
             result.page = 0
             const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.previous).to.equal(undefined)
+            expect(paginationResult.prevLink).to.equal(undefined)
         })
 
         it('should return 3 as next if currentPage is 2', function() {
             result.page = 1
             const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.next!.href).to.equal('/content-management/search?p=3')
+            expect(paginationResult.nextLink).to.equal('/content-management/search?p=3')
         })
 
         it('should return null as next if currentPage is the last page', function() {
             result.page = 4
             const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.next).to.equal(undefined)
-        })
-
-        it('should return only the first page as current when currentPage is 1 and total pages is 1', function() {
-            result.page = 0
-            result.totalResults = 10
-            const paginationItems = [{ number: 1, url: '/content-management/search?p=1', current: true }]
-
-            const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.items).to.deep.equal(paginationItems)
+            expect(paginationResult.nextLink).to.equal(undefined)
         })
 
         it('should return 1,[2],3,...,10 as items if currentPage is 2 and total pages is 10', function() {
             result.page = 1
             result.totalResults = 100
             const paginationItems = [
-                { number: 1, url: '/content-management/search?p=1' },
-                { number: 2, url: '/content-management/search?p=2', current: true },
-                { number: 3, url: '/content-management/search?p=3' },
+                { number: 1, href: '/content-management/search?p=1', current: false },
+                { number: 2, href: undefined, current: true },
+                { number: 3, href: '/content-management/search?p=3', current: false },
                 { ellipsis: true },
-                { number: 10, url: '/content-management/search?p=10' }
+                { number: 10, href: '/content-management/search?p=10', current: false }
             ]
 
             const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.items).to.deep.equal(paginationItems)
+            expect(paginationResult.numberedPages).to.deep.equal(paginationItems)
         })
 
         it('should return 1,...,8,[9],10 as items if currentPage is 9 and total pages is 10', function() {
             result.page = 8
             result.totalResults = 100
             const paginationItems = [
-                { number: 1, url: '/content-management/search?p=1' },
+                { number: 1, href: '/content-management/search?p=1', current: false  },
                 { ellipsis: true },
-                { number: 8, url: '/content-management/search?p=8' },
-                { number: 9, url: '/content-management/search?p=9', current: true },
-                { number: 10, url: '/content-management/search?p=10' }
+                { number: 8, href: '/content-management/search?p=8', current: false  },
+                { number: 9, href: undefined, current: true },
+                { number: 10, href: '/content-management/search?p=10', current: false  }
             ]
 
             const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.items).to.deep.equal(paginationItems)
+            expect(paginationResult.numberedPages).to.deep.equal(paginationItems)
         })
 
         it('should return 1,...,9,[10] as items if currentPage is 10 and total pages is 10', function() {
             result.page = 9
             result.totalResults = 100
             const paginationItems = [
-                { number: 1, url: '/content-management/search?p=1' },
+                { number: 1, href: '/content-management/search?p=1', current: false  },
                 { ellipsis: true },
-                { number: 9, url: '/content-management/search?p=9'},
-                { number: 10, url: '/content-management/search?p=10', current: true  }
+                { number: 9, href: '/content-management/search?p=9', current: false },
+                { number: 10, href: undefined, current: true  }
             ]
 
             const paginationResult = pagination.getPagination(q, result)
-            expect(paginationResult.pagination.items).to.deep.equal(paginationItems)
+            expect(paginationResult.numberedPages).to.deep.equal(paginationItems)
         })
     })
 
