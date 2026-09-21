bcd188f1f020dccf2102fae32e5dc37ca8a29f63
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-09-21 13:35:37 +0100

    Clear the category page cache when updating the taxonomy

--- src/csl-service/model/learning/learningTag/learningTagCacheManager.ts
+++ src/csl-service/model/learning/learningTag/learningTagCacheManager.ts
@@ -5,11 +5,18 @@ import {FormattedTaxonomyItem} from '../../../../lib/taxonomy/formattedTaxonomyI
 import {CacheableObjectCache} from '../../../../lib/cache/cacheableObjectCache'
 import {FormattedTaxonomyItemList} from '../../../../lib/taxonomy/formattedTaxonomyItemList'
 import {FetchedRedisCache} from '../../../../lib/cache/fetchedRedisCache'
+import {LearningCategoryCache} from '../../../learningCategoryCache'
 
 
 export class LearningTagCacheManager extends TaxonomyItemCacheManager<LearningTag, FormattedTaxonomyItem, TaxonomyTree> {
 
-	constructor(cacheableObjectCache: CacheableObjectCache<LearningTag>, formattedNameCache: CacheableObjectCache<FormattedTaxonomyItemList<FormattedTaxonomyItem>>, treeCache: FetchedRedisCache<TaxonomyTree>) {
+	constructor(cacheableObjectCache: CacheableObjectCache<LearningTag>, formattedNameCache: CacheableObjectCache<FormattedTaxonomyItemList<FormattedTaxonomyItem>>, treeCache: FetchedRedisCache<TaxonomyTree>,
+				private learningCategoryCache: LearningCategoryCache) {
 		super(cacheableObjectCache, formattedNameCache, treeCache)
+	}
+
+	async update(updatedObject: LearningTag): Promise<void> {
+		await super.update(updatedObject);
+		await this.learningCategoryCache.deleteAllIds()
 	}
 }
